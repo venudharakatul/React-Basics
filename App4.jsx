a2cfd0a1cb5641ff31da9da7a0a87c6f9fa1a6bd
Atul Gupta <136560060+venudharakatul@users.noreply.github.com>
2024-03-15 23:01:20 +0530

    Create App4.jsx

---
+++ App4.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+const App4 = () =>{
+    return(
+        <>
+        <div className="main_div">
+            <div className="centre_div">
+            <br>
+            <h1> ToDoList</h1>
+            </br>
+            <input type="text" placeholder="Add a items"/>
+            <button> + </button>
+            </div>
+        </div>
+        </>
+        );
+};
+export default App4;
