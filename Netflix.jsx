e314ff6089944be72169c22e989af1ea22e6ecbc
Atul Gupta <136560060+venudharakatul@users.noreply.github.com>
2024-03-15 23:06:57 +0530

    Create Netflix.jsx

---
+++ Netflix.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Card from "./Cards";
+import Sdata from "./Sdata";
+
+const Netflix = () => {
+  return (
+    <Card
+      key={Sdata[1].id}
+      imgsrc={Sdata[1].imgsrc}
+      title={Sdata[1].title}
+      sname={Sdata[1].title}
+      link={Sdata[1].link}
+    />
+  );
+}
+export default Netflix;
