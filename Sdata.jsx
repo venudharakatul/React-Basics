278ebd37b7d15081c0b9c947910b21dac878dd1f
Atul Gupta <136560060+venudharakatul@users.noreply.github.com>
2024-03-15 23:09:39 +0530

    Create Sdata.jsx

---
+++ Sdata.jsx
@@ -0,0 +1,38 @@
+const Sdata = [
+    {
+    id:1,
+    imgsrc:"https://wallpapercave.com/w/wp4995606.jpg",
+    title:"A Netflix original Series",
+    sname:"Dark",
+    link:"https://www.netflix.com/in/title/80100172?source=35"
+},
+{
+    id:2,
+    imgsrc:"https://wallpapercave.com/w/wp4995606.jpg",
+    title:"A Netflix original Series",
+    sname:"Vampire Diaries",
+    link:"https://www.netflix.com/in/title/80100172?source=35"
+},
+{
+    id:3,
+    imgsrc:"https://wallpapercave.com/w/wp4995606.jpg",
+    title:"A Netflix original Series",
+    sname:"Originals",
+    link:"https://www.netflix.com/in/title/80100172?source=35"
+},
+{
+    id:4,
+    imgsrc:"https://wallpapercave.com/w/wp4995606.jpg",
+    title:"A Netflix original Series",
+    sname:"Wednesday",
+    link:"https://www.netflix.com/in/title/80100172?source=35"
+}
+
+]
+export default Sdata;
+{/* <script>
+const oldarr= ["vinod", "bahadur", "thapa"]
+const newarr= oldarr.map(function(cvalue,i,arr){
+    console.log(arr);
+});
+</script> */}
